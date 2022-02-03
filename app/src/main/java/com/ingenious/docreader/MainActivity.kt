784cf349246681443b507f7fadad28bc0bfd8968
Mobeen Riaz <mobeenriaz89@gmail.com>
2022-02-03 22:12:32 +0500

    cleaning code

--- app/src/main/java/com/ingenious/docreader/MainActivity.kt
+++ app/src/main/java/com/ingenious/docreader/MainActivity.kt
@@ -1,63 +1,57 @@
 package com.ingenious.docreader
 
 import android.Manifest
-import android.app.Activity
-import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.graphics.PathUtils
-import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
 
-    private val REQUEST_CODE_PERMISSION_STORAGE = 1
+    private val requestCodePermissionStorage = 1
 
     private lateinit var btnOpenFile: Button
-    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
-        val path: String? = uri?.path
-        Toast.makeText(this,"File path: $path",Toast.LENGTH_LONG).show()
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        btnOpenFile = findViewById(R.id.btnOpenFile)
+        btnOpenFile.setOnClickListener { checkForPermission() }
     }
 
-        override fun onCreate(savedInstanceState: Bundle?) {
-            super.onCreate(savedInstanceState)
-            setContentView(R.layout.activity_main)
-            btnOpenFile = findViewById(R.id.btnOpenFile)
-            btnOpenFile.setOnClickListener { checkForPermission() }
-        }
 
-
-    private fun checkForPermission(){
+    private fun checkForPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            when{
-                ContextCompat.checkSelfPermission(this,
+            when {
+                ContextCompat.checkSelfPermission(
+                    this,
                     Manifest.permission.READ_EXTERNAL_STORAGE
                 ) == PackageManager.PERMISSION_GRANTED -> {
                     openDoc()
                 }
-                shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) ->{
-                    Toast.makeText(this,"Need storage permission",Toast.LENGTH_SHORT)
+                shouldShowRequestPermissionRationale(Manifest.permission.READ_EXTERNAL_STORAGE) -> {
+                    Toast.makeText(this, "Need storage permission", Toast.LENGTH_SHORT).show()
                 }
                 else -> {
-                    requestPermissions(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    ,REQUEST_CODE_PERMISSION_STORAGE)
+                    requestPermissions(
+                        arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+                        requestCodePermissionStorage
+                    )
                 }
             }
         }
     }
+
     private fun openDoc() {
-        val myIntent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
-            addCategory(Intent.CATEGORY_OPENABLE)
-            type = "application/pdf"
-        }
+        val getContent =
+            registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+                val path: String? = uri?.path
+            }
         getContent.launch("application/pdf")
 
     }
@@ -68,12 +62,12 @@ class MainActivity : AppCompatActivity() {
         grantResults: IntArray
     ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when(requestCode){
-            REQUEST_CODE_PERMISSION_STORAGE -> {
-                if((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)){
+        when (requestCode) {
+            requestCodePermissionStorage -> {
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
                     openDoc()
-                }else{
-                    Toast.makeText(this,"Permission not granted",Toast.LENGTH_SHORT)
+                } else {
+                    Toast.makeText(this, "Permission not granted", Toast.LENGTH_SHORT).show()
                 }
                 return
             }
